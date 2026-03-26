a64ceb0f8efad180821f48c788d0c816420d8c5c
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-03-26 10:49:39 -0300

    fix(api): habilitar CORS para permitir integração com frontend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from backend.chatbot.engine import ChatbotEngine
 
 app = Flask(__name__)
+
+CORS(app)
+
 chatbot = ChatbotEngine(user_id="vinicius")
 
 @app.route("/chat", methods=["POST"])
